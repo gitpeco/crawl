67d72cdd34c2d41acdc9289841ebbfb45ae83494
gitpeco <znm9110@163.com>
2025-02-23 23:37:45 +0800

    登录界面和注册界面出现交互404的情况得解决

--- pythonProject/crawler_project/crawler_project/urls.py
+++ pythonProject/crawler_project/crawler_project/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('main.urls')),
+    path('register/', include('main.urls', namespace='auth_register')),
 ]
--- pythonProject/crawler_project/main/views.py
+++ pythonProject/crawler_project/main/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .crawler import run_crawler
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required
 def home(request):
@@ -16,3 +18,14 @@ def home(request):
 def search(request):
     results = run_crawler(query=request.GET.get('query', ''))
     return render(request, 'main/search.html', {'results': results})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # 注册成功后跳转到登录页面
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
